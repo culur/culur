2430979eae2626c4368034410007ce518ec324fc
phamhongphuc <phamhongphuc.info@gmail.com>
2024-05-26 14:22:22 +0700

    feat(package): add `config-prettier`

---
+++ packages/config-prettier/src/factory.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it, vi } from 'vitest';
+import prettier from 'prettier';
+import definePrettierConfig from './factory';
+
+describe('platform', () => {
+  vi.mock('os');
+
+  it.each([
+    { platform: 'win32', endOfLine: 'crlf' },
+    { platform: 'linux', endOfLine: 'lf' },
+  ])('platform is $platform', async ({ platform, endOfLine }) => {
+    const os = await import('node:os');
+    os.platform = vi.fn().mockReturnValue(platform);
+
+    const config = definePrettierConfig();
+    expect(config.endOfLine).toBe(endOfLine);
+  });
+});
+
+describe('create config', () => {
+  it('is valid config', async () => {
+    const config = definePrettierConfig();
+    const code = 'const a = \n 123';
+    const formattedCode = await prettier.format(code, {
+      ...config,
+      parser: 'typescript',
+    });
+    expect(formattedCode.trim()).toBe('const a = 123;');
+  });
+
+  it('can override config', () => {
+    const config = definePrettierConfig({
+      tabWidth: 4,
+      overrides: [
+        {
+          files: ['custom.ts'],
+          options: { printWidth: 160 },
+        },
+      ],
+    });
+
+    expect(config.tabWidth).toBe(4);
+    expect(
+      config.overrides?.find(override => override.files.includes('custom.ts'))
+        ?.options?.printWidth,
+    ).toBe(160);
+  });
+});
---
+++ packages/config-prettier/src/factory.ts
@@ -0,0 +1,61 @@
+import { platform } from 'node:os';
+import { defineObject } from '@culur/types';
+import type { Config } from 'prettier';
+
+export default function definePrettierConfig(config: Config = {}): Config {
+  const isWin = platform() === 'win32';
+
+  const defaultConfig = defineObject<Config>()({
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    htmlWhitespaceSensitivity: 'ignore',
+    printWidth: 80,
+    endOfLine: isWin ? 'crlf' : 'lf',
+    overrides: [
+      {
+        files: [
+          // alphabetical order
+          '.babelrc', // (alias for .babelrc.json) https://babeljs.io/docs/config-files#supported-file-extensions
+          '.czrc', // https://github.com/commitizen/cz-cli
+          '.eslintrc', // https://eslint.org/docs/latest/use/configure/configuration-files-deprecated
+          '.gqlconfig', // https://marketplace.visualstudio.com/items?itemName=kumar-harsh.graphql-for-vscode
+          '.ncurc', // https://github.com/raineorshine/npm-check-updates?tab=readme-ov-file#config-file
+          '.prettierrc', // (JSON or YAML) https://prettier.io/docs/en/configuration.html
+          '.releaserc', // https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration-file
+          '.stylelintrc', // https://stylelint.io/user-guide/configure/
+          '*.code-workspace', // https://code.visualstudio.com/docs/editor/workspaces#_multiroot-workspaces
+        ],
+        options: { parser: 'json' },
+      },
+      {
+        files: [
+          // alphabetical order
+          '*.md',
+          '*.ya?ml',
+          'package*.json',
+        ],
+        options: { tabWidth: 2 },
+      },
+      {
+        files: [
+          // alphabetical order
+          '*.sh',
+        ],
+        options: { endOfLine: 'lf' },
+      },
+    ],
+  });
+
+  const { overrides, ...options } = config;
+  const { overrides: defaultOverrides, ...defaultOptions } = defaultConfig;
+
+  return {
+    ...defaultOptions,
+    ...options,
+    overrides: [...defaultOverrides, ...(overrides ?? [])],
+  };
+}

3a5d2b0830dd689b2c08315d22d356e6585cefee
phamhongphuc <phamhongphuc.info@gmail.com>
2024-07-11 23:02:17 +0700

    feat(config-vite): Update `test.coverage`

--- packages/config-vite/src/factory/options-vitest.ts
+++ packages/config-vite/src/factory/options-vitest.ts
@@ -1,9 +1,13 @@
+import type { BaseCoverageOptions } from 'vitest';
 import type { VitestInlineConfig } from '~/types';
 
 export const defineConfigTest = (
   test: boolean | VitestInlineConfig | undefined,
 ): VitestInlineConfig | undefined => {
   if (test === undefined || test === false) return undefined;
+  const { coverage, ...testObject } =
+    typeof test === 'object' ? test : ({} as VitestInlineConfig);
+
   return {
     include: ['**/*\\.{test,test-d}.ts'],
     typecheck: {
@@ -13,7 +17,8 @@ export const defineConfigTest = (
     coverage: {
       enabled: true,
       reporter: ['text', 'html', 'json', 'lcov'],
+      ...((coverage ?? {}) as BaseCoverageOptions),
     },
-    ...(typeof test === 'object' ? test : {}),
+    ...testObject,
   };
 };

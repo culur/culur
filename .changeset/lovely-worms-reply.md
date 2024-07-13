f6cf51da5d27bb40f7a53073f92662a7ebdc4e86
phamhongphuc <phamhongphuc.info@gmail.com>
2024-07-13 18:23:43 +0700

    feat(config-eslint): rename to `culur/filename/rules`

---
+++ .changeset/lovely-worms-reply.md
@@ -0,0 +1,5 @@
+---
+'@culur/config-eslint': patch
+---
+
+Rename to `culur/filename/rules`.
--- packages/config-eslint/src/rules/filename.test.ts
+++ packages/config-eslint/src/rules/filename.test.ts
@@ -1,7 +1,7 @@
 import { expectTypeOf, it } from 'vitest';
 import type { TypedFlatConfigItem } from '@antfu/eslint-config';
-import { unicornFilenameCase } from './unicorn-filename-case';
+import { filenameRules } from './filename';
 
 it('should be valid rule', async () => {
-  expectTypeOf(unicornFilenameCase).toEqualTypeOf<TypedFlatConfigItem>();
+  expectTypeOf(filenameRules).toEqualTypeOf<TypedFlatConfigItem>();
 });

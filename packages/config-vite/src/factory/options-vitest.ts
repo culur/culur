ebb43b989ff697440a397e865cac185770fae62a
phamhongphuc <phamhongphuc.info@gmail.com>
2024-07-17 08:11:24 +0700

    feat(config-vite): update `test.typecheck`

--- packages/config-vite/src/factory/options-vitest.ts
+++ packages/config-vite/src/factory/options-vitest.ts
@@ -1,23 +1,27 @@
-import type { BaseCoverageOptions } from 'vitest';
+import type { BaseCoverageOptions, TypecheckConfig } from 'vitest';
 import type { VitestInlineConfig } from '~/types';
 
 export const defineConfigTest = (
   test: boolean | VitestInlineConfig | undefined,
 ): VitestInlineConfig | undefined => {
   if (test === undefined || test === false) return undefined;
-  const { coverage, ...testObject } =
+  const { coverage, typecheck, ...testObject } =
     typeof test === 'object' ? test : ({} as VitestInlineConfig);
+
+  const typecheckOptions: Partial<TypecheckConfig> = typecheck ?? {};
+  const coverageOptions: BaseCoverageOptions = coverage ?? {};
 
   return {
     include: ['**/*\\.{test,test-d}.ts'],
     typecheck: {
       tsconfig: './tsconfig.json',
       include: ['**/*.test-d.ts'],
+      ...typecheckOptions,
     },
     coverage: {
       enabled: true,
       reporter: ['text', 'html', 'json', 'lcov'],
-      ...((coverage ?? {}) as BaseCoverageOptions),
+      ...coverageOptions,
     },
     ...testObject,
   };

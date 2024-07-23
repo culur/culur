6ade7062d2df88898eb00dcb522f207fc8c8dda2
phamhongphuc <phamhongphuc.info@gmail.com>
2024-07-23 15:42:40 +0700

    chore(root/eslint): override `yarnrc-yml`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,26 @@
 import defineConfig from '@culur/config-eslint';
+import { yamlOrder, yamlYarnrcSortKeys } from '@culur/config-eslint/rules';
 
 export default defineConfig({
+  name: 'workspace/ignore',
   ignores: ['templates/template-vue-vite/**'],
+}).override('culur/yarnrc-yml/rules', {
+  files: ['.yarnrc.yml'],
+  rules: {
+    'yaml/sort-keys': [
+      'error',
+      {
+        ...yamlYarnrcSortKeys.packageExtensions.packages,
+        order: [
+          { order: yamlOrder, keyPattern: /^@actions\/.+/.source },
+          { order: yamlOrder, keyPattern: /eslint-.+/.source },
+          { order: yamlOrder, keyPattern: /vue/.source },
+          { order: yamlOrder },
+        ],
+      },
+      yamlYarnrcSortKeys.packageExtensions.packages,
+      yamlYarnrcSortKeys.packageExtensions.dependenciesTypes,
+      yamlYarnrcSortKeys.packageExtensions.childPackages,
+    ],
+  },
 });

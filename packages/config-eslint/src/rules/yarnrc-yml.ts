ff62273d6f872ceca3669486447c428ad068000d
phamhongphuc <phamhongphuc.info@gmail.com>
2024-07-23 12:29:52 +0700

    refactor(config-eslint): create index export for `overrides` & `rules` folders

--- packages/config-eslint/src/rules/yarnrc-yml.ts
+++ packages/config-eslint/src/rules/yarnrc-yml.ts
@@ -1,4 +1,37 @@
-import type { TypedFlatConfigItem } from '@antfu/eslint-config';
+import type { Rules, TypedFlatConfigItem } from '@antfu/eslint-config';
+import type { Linter } from 'eslint';
+
+type YamlSortKeys =
+  NonNullable<Rules['yaml/sort-keys']> extends Linter.RuleEntry<infer F>
+    ? F
+    : never;
+
+type YamlSortKeysRecord = YamlSortKeys[0];
+
+export const yamlOrder = {
+  type: 'asc',
+  caseSensitive: false,
+  natural: true,
+} as const;
+
+export const yamlYarnrcSortKeys = {
+  packageExtensions: {
+    packages: {
+      pathPattern: /^packageExtensions$/.source,
+      order: yamlOrder,
+    },
+    dependenciesTypes: {
+      pathPattern: /^packageExtensions(?:\[".+"\]|\.[^.]+)$/.source,
+      order: yamlOrder,
+    },
+    childPackages: {
+      pathPattern:
+        /^packageExtensions(?:\[".+"\]|\.[^.]+)\.(?:dependencies|peerDependencies|peerDependenciesMeta)$/
+          .source,
+      order: yamlOrder,
+    },
+  },
+} satisfies Record<string, Record<string, YamlSortKeysRecord>>;
 
 export const yamlYarnrcRules: TypedFlatConfigItem = {
   name: 'culur/yarnrc-yml/rules',
@@ -6,32 +39,9 @@ export const yamlYarnrcRules: TypedFlatConfigItem = {
   rules: {
     'yaml/sort-keys': [
       'error',
-      {
-        pathPattern: /^packageExtensions$/.source,
-        order: {
-          type: 'asc',
-          caseSensitive: false,
-          natural: true,
-        },
-      },
-      {
-        pathPattern: /^packageExtensions(?:\[".+"\]|\.[^.]+)$/.source,
-        order: [
-          'dependencies', //
-          'peerDependencies',
-          'peerDependenciesMeta',
-        ],
-      },
-      {
-        pathPattern:
-          /^packageExtensions(?:\[".+"\]|\.[^.]+)\.(?:dependencies|peerDependencies|peerDependenciesMeta)$/
-            .source,
-        order: {
-          type: 'asc',
-          caseSensitive: false,
-          natural: true,
-        },
-      },
+      yamlYarnrcSortKeys.packageExtensions.packages,
+      yamlYarnrcSortKeys.packageExtensions.dependenciesTypes,
+      yamlYarnrcSortKeys.packageExtensions.childPackages,
     ],
   },
 };

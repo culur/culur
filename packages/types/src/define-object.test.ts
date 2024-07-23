262fe814f37f3ccdf26275149a9ff38b0553ebdd
phamhongphuc <phamhongphuc.info@gmail.com>
2024-07-23 21:54:51 +0700

    feat(types/define-object): add generic `AutoComplete`

--- packages/types/src/define-object.test.ts
+++ packages/types/src/define-object.test.ts
@@ -2,7 +2,7 @@ import { describe, expectTypeOf, it } from 'vitest';
 import { defineObject } from './define-object';
 
 describe('defineObject', () => {
-  it('should be valid type', () => {
+  it('auto completion = default = false', () => {
     const object = defineObject<{
       foo?: string | number;
       bar?: boolean;
@@ -13,5 +13,22 @@ describe('defineObject', () => {
     expectTypeOf(object).toMatchTypeOf<{
       foo: string;
     }>();
+  });
+
+  it('auto completion = default = true', () => {
+    const object = defineObject<
+      {
+        foo?: string | number;
+        bar?: boolean;
+      },
+      true
+    >()({
+      foo: 'foo',
+    });
+
+    expectTypeOf(object).toMatchTypeOf<
+      | { foo?: string | number; bar?: boolean } //
+      | { foo: string }
+    >();
   });
 });
--- packages/types/src/define-object.ts
+++ packages/types/src/define-object.ts
@@ -1,4 +1,8 @@
 export const defineObject =
-  <TBaseObject>() =>
-  <TActualObject extends TBaseObject>(object: TActualObject) =>
+  <TBaseObject, TAutoComplete extends boolean = false>() =>
+  <TActualObject extends TBaseObject>(
+    object: TAutoComplete extends true
+      ? TActualObject | TBaseObject
+      : TActualObject,
+  ) =>
     object;

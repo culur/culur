6e8c42f4fcf2b890ad3f1e70d7d5c95c1e663fe5
phamhongphuc <phamhongphuc.info@gmail.com>
2024-09-14 21:16:12 +0700

    test(changesets-changelog-github): update unit test

---
+++ .changeset/grumpy-carrots-exercise.md
@@ -0,0 +1,5 @@
+---
+'@culur/changesets-changelog-github': patch
+---
+
+Test: update unit test
--- packages/changesets-changelog-github/src/__tests__/mock-github-info.ts
+++ packages/changesets-changelog-github/src/__tests__/mock-github-info.ts
@@ -1,38 +1,31 @@
 import type { MockRecord } from './types';
-import * as githubInfo from '@culur/changesets-github-info';
-import { assert, vi } from 'vitest';
+import { assert } from 'vitest';
 import { getCommit, getPull, getUser } from './mock-links';
 
-export const mockGithubInfo = (...records: MockRecord[]) => {
-  vi.spyOn(githubInfo, 'getCommitInfo').mockImplementation(
-    async ({ repo, commitHash }) => {
-      const record = records.find(
-        r => r.repo === repo && r.commitHash === commitHash,
-      );
-      assert(!!record && !!record.commitHash);
-
-      return {
-        user: getUser(record),
-        commit: getCommit(record),
-        pullRequest: record.pullRequestNumber ? getPull(record) : null,
-      };
-    },
-  );
-
-  vi.spyOn(githubInfo, 'getPullRequestInfo').mockImplementation(
-    async ({ repo, pullRequestNumber }) => {
-      const record = records.find(
-        record =>
-          record.pullRequestNumber === pullRequestNumber &&
-          record.repo === repo,
-      );
-      assert(!!record && !!record.pullRequestNumber);
-
-      return {
-        user: getUser(record),
-        commit: record.commitHash ? getCommit(record) : null,
-        pullRequest: getPull(record),
-      };
-    },
-  );
-};
+export const changesetsGithubInfo = (
+  ...mockRecords: MockRecord[]
+): Awaited<typeof import('@culur/changesets-github-info')> => ({
+  async getCommitInfo({ repo, commitHash }) {
+    const record = mockRecords.find(
+      r => r.repo === repo && r.commitHash === commitHash,
+    );
+    assert(!!record && !!record.commitHash, 'not found mock record');
+    return {
+      user: getUser(record),
+      commit: getCommit(record),
+      pullRequest: record.pullRequestNumber ? getPull(record) : null,
+    };
+  },
+  async getPullRequestInfo({ repo, pullRequestNumber }) {
+    const record = mockRecords.find(
+      record =>
+        record.pullRequestNumber === pullRequestNumber && record.repo === repo,
+    );
+    assert(!!record && !!record.pullRequestNumber, 'not found mock record');
+    return {
+      user: getUser(record),
+      commit: record.commitHash ? getCommit(record) : null,
+      pullRequest: getPull(record),
+    };
+  },
+});

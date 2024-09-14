ff7f9d88322874e4b68bf0a8121c14d33137d19d
phamhongphuc <phamhongphuc.info@gmail.com>
2024-09-14 23:07:09 +0700

    chore(root): update build & linter scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,19 @@
 import defineConfig from '@culur/config-eslint';
-import { yamlOrder, yamlYarnrcSortKeys } from '@culur/config-eslint/rules';
 
-export default defineConfig({
-  name: 'workspace/ignore',
-  ignores: ['templates/template-vue-vite/**'],
-}).override('culur/yarnrc-yml/rules', {
-  files: ['.yarnrc.yml'],
-  rules: {
-    'yaml/sort-keys': [
-      'error',
-      {
-        ...yamlYarnrcSortKeys.packageExtensions.packages,
-        order: [
-          { order: yamlOrder, keyPattern: /^@actions\/.+/.source },
-          { order: yamlOrder, keyPattern: /eslint-.+/.source },
-          { order: yamlOrder, keyPattern: /vue/.source },
-          { order: yamlOrder },
-        ],
-      },
-      yamlYarnrcSortKeys.packageExtensions.packages,
-      yamlYarnrcSortKeys.packageExtensions.dependenciesTypes,
-      yamlYarnrcSortKeys.packageExtensions.childPackages,
+export default defineConfig(
+  {
+    vue: true,
+    react: true,
+    typescript: true,
+  },
+  {
+    // ignore projects that have custom lint and test scripts
+    name: 'projects/ignore',
+    ignores: [
+      'apps/*/**', //
+      'packages/*/**',
+      'scripts/*/**',
+      'templates/*/**',
     ],
   },
-});
+);

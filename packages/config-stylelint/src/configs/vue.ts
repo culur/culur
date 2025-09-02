1e046aac47a0ca970c2e63654532376b17c233ee
phamhongphuc <phamhongphuc.info@gmail.com>
2025-09-02 21:43:21 +0700

    feat(config-stylelint): remove `at-rule-no-deprecated` rule

--- packages/config-stylelint/src/configs/vue.ts
+++ packages/config-stylelint/src/configs/vue.ts
@@ -1,6 +1,5 @@
 import type { Packages } from '@culur/utils-packages';
 import type { Config } from 'stylelint';
-import { atRuleNoDeprecated } from '~/rules/at-rule-no-deprecated';
 
 export const configVue = (packages: Partial<Packages>): Config => ({
   extends: [
@@ -8,7 +7,4 @@ export const configVue = (packages: Partial<Packages>): Config => ({
       ? 'stylelint-config-standard-vue/scss'
       : 'stylelint-config-standard-vue',
   ],
-  rules: {
-    ...atRuleNoDeprecated(packages), // override this rule in `stylelint-config-standard-vue/scss` & `stylelint-config-standard-vue`
-  },
 });

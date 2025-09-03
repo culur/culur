e19b0cd983ddcfd8f14235e01584a76f2ff884fa
phamhongphuc <phamhongphuc.info@gmail.com>
2025-09-03 08:13:47 +0700

    feat(config-eslint): update types to match changes in `@antfu/eslint-config@5`

--- packages/config-eslint/src/types/index.ts
+++ packages/config-eslint/src/types/index.ts
@@ -5,18 +5,27 @@ import type {
 } from '@antfu/eslint-config';
 import type { Linter } from 'eslint';
 
+export interface ExtraRules {}
+
+export type TypedFlatConfigItemCustom = Omit<TypedFlatConfigItem, 'rules'> & {
+  rules?: Omit<Rules, keyof ExtraRules> & Partial<ExtraRules>;
+};
+
 export type OverrideConfig = {
-  name: NonNullable<TypedFlatConfigItem['name']>;
+  name: NonNullable<TypedFlatConfigItemCustom['name']>;
 } & (
-  | { partialConfig: TypedFlatConfigItem } // merge with the original config
-  | { entireConfig: () => Awaitable<TypedFlatConfigItem> } // replace the entire config
+  | { partialConfig: TypedFlatConfigItemCustom } // merge with the original config
+  | { entireConfig: () => Awaitable<TypedFlatConfigItemCustom> } // replace the entire config
 );
 
 export const defineOverride = (config: OverrideConfig) =>
   [
     config.name,
     'partialConfig' in config ? config.partialConfig : config.entireConfig,
-  ] as const;
+  ] as [
+    name: string,
+    config: TypedFlatConfigItem | (() => Awaitable<TypedFlatConfigItem>),
+  ];
 
 export type RuleEntry<T extends keyof Rules> = Rules[T] extends
   | Linter.RuleEntry<infer F>

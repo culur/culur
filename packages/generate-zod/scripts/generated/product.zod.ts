a34aeaa6b50151ab53d3ab0a50bc4dc6ca1e7962
phamhongphuc <phamhongphuc.info@gmail.com>
2025-09-21 16:15:52 +0700

    test(generate-zod): update unit tests

--- packages/generate-zod/scripts/generated/product.zod.ts
+++ packages/generate-zod/scripts/generated/product.zod.ts
@@ -3,9 +3,9 @@ import { PermissionLevel, Status } from '~/__tests__/samples/product-enum';
 import { isValidAgainstSchema } from '~/is-valid-against-schema';
 
 // Basic enum with string values
-export const statusRawSchema = z.nativeEnum(Status);
+export const statusRawSchema = z.enum(Status);
 
 export const isStatusRaw = isValidAgainstSchema<Status>(statusRawSchema);
 
 // Enum without explicitly assigned values (defaults to 0, 1, 2, ...)
-export const permissionLevelSchema = z.nativeEnum(PermissionLevel);
+export const permissionLevelSchema = z.enum(PermissionLevel);
